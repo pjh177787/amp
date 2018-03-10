a22c8328f70b018e015d647b9ff85e017faeb6b0
Aperture <pjh177787@users.noreply.github.com>
2018-03-10 01:02:27 -0600

    随缘

--- MP2/gomoku/board.py
+++ MP2/gomoku/board.py
@@ -28,6 +28,11 @@ class Board:
                     spaces.append((row, cell))
         return spaces
     
+    def is_movable(self, row, col):
+        if row < 0 or row > 6 or col < 0 or col > 6:
+            return False
+        return self.board[row][col] == '.'
+    
     def make_move(self, player, row, col):
         if not self.board[row][col] == '.':
             print('Can\'t  play here.')
@@ -81,3 +86,107 @@ class Board:
                     return row, col
         print('Not found')
         return -1, -1
+    
+    def l2r_has_four(self, player):
+        if not (player == 'x' or player == 'o'):
+            print('I don\'t know you.')
+            return -1, -1
+        bd = self.get_board()
+        for col in range(4):
+            for row in range(4):
+                flag = True
+                for i in range(4):
+                    if not bd[row+i, col+i] == player:
+                        flag = False
+                        break
+                if flag:
+                    return row, col
+        print('Not found')
+        return -1, -1
+    
+    def r2l_has_four(self, player):
+        if not (player == 'x' or player == 'o'):
+            print('I don\'t know you.')
+            return -1, -1
+        bd = self.get_board()
+        for col in range(3, 7):
+            for row in range(4):
+                flag = True
+                for i in range(4):
+                    if not bd[row+i, col-i] == player:
+                        flag = False
+                        break
+                if flag:
+                    return row, col
+        print('Not found')
+        return -1, -1
+    
+    def row_has_three(self, player):
+        if not (player == 'x' or player == 'o'):
+            print('I don\'t know you.')
+            return -1, -1
+        bd = self.get_board()
+        for row in range(7):
+            for col in range(5):
+                flag = True
+                for cell in bd[row, col:col+3]:
+                    if not cell == player:
+                        flag = False
+                        break
+                if flag:       
+                    return row, col
+        print('Not found')
+        return -1, -1
+    
+    def col_has_three(self, player):
+        if not (player == 'x' or player == 'o'):
+            print('I don\'t know you.')
+            return -1, -1
+        bd = self.get_board()
+        for col in range(7):
+            for row in range(5):
+                flag = True
+                for cell in bd[row:row+3, col]:
+                    if not cell == player:
+                        flag = False
+                        break
+                if flag:       
+                    return row, col
+        print('Not found')
+        return -1, -1
+    
+    def l2r_has_three(self, player):
+        if not (player == 'x' or player == 'o'):
+            print('I don\'t know you.')
+            return -1, -1
+        bd = self.get_board()
+        for col in range(5):
+            for row in range(5):
+                flag = True
+                for i in range(3):
+                    if not bd[row+i, col+i] == player:
+                        flag = False
+                        break
+                if flag:
+                    return row, col
+        print('Not found')
+        return -1, -1
+    
+    def r2l_has_three(self, player):
+        if not (player == 'x' or player == 'o'):
+            print('I don\'t know you.')
+            return -1, -1
+        bd = self.get_board()
+        for col in range(2, 7):
+            for row in range(5):
+                flag = True
+                for i in range(3):
+                    if not bd[row+i, col-i] == player:
+                        flag = False
+                        break
+                if flag:
+                    return row, col
+        print('Not found')
+        return -1, -1
+                
+        
--- MP2/gomoku/reflex.py
+++ MP2/gomoku/reflex.py
@@ -10,7 +10,78 @@ class Reflex:
         else:
             print('I don\'t know you.')
             return -1, -1
-        return 1, 1
+        
+        row, col = board.row_has_four(player)
+        if (row, col) == (-1, -1):
+            row, col = board.col_has_four(player)
+            if (row, col) == (-1, -1):
+                row, col = board.l2r_has_four(player)
+                if (row, col) == (-1, -1):
+                    row, col = board.r2l_has_four(player)
+                    if (row, col) == (-1, -1):
+                        pass
+                    elif board.is_movable(row + 4, col - 4):
+                        return row + 4, col - 4
+                    elif board.is_movable(row - 1, col + 1):
+                        return row - 1, col + 1
+                elif board.is_movable(row - 1, col - 1):
+                    return row - 1, col - 1
+                elif board.is_movable(row + 4, col + 4):
+                    return row + 4, col + 4
+            elif board.is_movable(row + 4, col):
+                return row + 4, col
+            elif board.is_movable(row - 1, col):
+                return row - 1, col
+        elif board.is_movable(row, col - 1):
+            return row, col - 1
+        elif board.is_movable(row, col + 4):
+            return row, col + 4
+        
+        row, col = board.row_has_four(opponent)
+        if (row, col) == (-1, -1):
+            row, col = board.col_has_four(opponent)
+            if (row, col) == (-1, -1):
+                row, col = board.l2r_has_four(opponent)
+                if (row, col) == (-1, -1):
+                    row, col = board.r2l_has_four(opponent)
+                    if (row, col) == (-1, -1):
+                        pass
+                    elif board.is_movable(row + 4, col - 4):
+                        return row + 4, col - 4
+                    elif board.is_movable(row - 1, col + 1):
+                        return row - 1, col + 1
+                elif board.is_movable(row - 1, col - 1):
+                    return row - 1, col - 1
+                elif board.is_movable(row + 4, col + 4):
+                    return row + 4, col + 4
+            elif board.is_movable(row + 4, col):
+                return row + 4, col
+            elif board.is_movable(row - 1, col):
+                return row - 1, col
+        elif board.is_movable(row, col - 1):
+            return row, col - 1
+        elif board.is_movable(row, col + 4):
+            return row, col + 4
+        
+        row, col = board.row_has_three(opponent)
+        if (row, col) == (-1, -1):
+            row, col = board.col_has_three(opponent)
+            if (row, col) == (-1, -1):
+                row, col = board.l2r_has_three(opponent)
+                if (row, col) == (-1, -1):
+                    row, col = board.r2l_has_three(opponent)
+                    if (row, col) == (-1, -1):
+                        pass
+                    elif board.is_movable(row + 3, col - 3) and board.is_movable(row - 1, col + 1):
+                        return row + 3, col - 3
+                elif board.is_movable(row - 1, col - 1) and board.is_movable(row + 3, col + 3):
+                    return row - 1, col - 1
+            elif board.is_movable(row + 3, col) and board.is_movable(row - 1, col):
+                return row + 3, col
+        elif board.is_movable(row, col - 1) and board.is_movable(row, col + 3):
+            return row, col - 1
+            
+        return -1, -1
         
         # test for consecutive four
         
