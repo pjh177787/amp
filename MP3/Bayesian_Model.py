7e3e99175684803110dfbf7bc40ef6ce36fc14aa
Aperture <pjh177787@users.noreply.github.com>
2018-04-08 09:51:13 -0500

    苔痕上阶绿，草色入帘青

--- MP3/Bayesian_Model.py
+++ MP3/Bayesian_Model.py
@@ -4,35 +4,36 @@ import matplotlib.pyplot as plt
 
 class Bayesian_Model:
     def __init__(self):
-        self.train_classes = [
+        self.training_classes = [
             [
                 [{'1':0, '0':0} for i in range(32)]
                 for i in range(32)]
             for i in range(10)]
-        self.test_classes =  [
+        self.training_labels = []
+        self.testing_classes =  [
             [
                 [' ' for i in range(32)]
                 for i in range(32)]
             for i in range(444)]
+        self.testing_labels = []
         self.confusion_matrix = [[0 for i in range(10)] for i in range(10)]
         self.count = np.zeros(10) # count of appearances of each number in the training sample
         self.priors = np.zeros(10)
 
     def parse_file(self, trainfile_name, testfile_name, smooth_factor):
         trainfile = open(trainfile_name, 'r')
-        label_list = []
         for line in trainfile:
             if len(line) < 32:
                 for ch in line:
                     if ch.isdigit():
-                        label_list.append(int(ch))
+                        self.training_labels.append(int(ch))
         trainfile.seek(0)
-        for label in label_list:
+        for label in self.training_labels:
             self.count[label] += 1
             for i in range(32):
                 image_line = trainfile.readline()
                 for j in range(32):
-                    self.train_classes[label][i][j][image_line[j]] += 1
+                    self.training_classes[label][i][j][image_line[j]] += 1
             image_line = trainfile.readline()
             for ch in image_line:
                 if ch.isdigit() and int(ch) != label:
@@ -43,21 +44,20 @@ class Bayesian_Model:
         self.prior()
 
         testfile = open(testfile_name, 'r')
-        label_list = []
         for line in testfile:
             if len(line) < 32:
                 for ch in line:
                     if ch.isdigit():
-                        label_list.append(int(ch))
+                        self.testing_labels.append(int(ch))
         testfile.seek(0)        
-        for idx in range(len(label_list)):
+        for idx in range(len(self.testing_labels)):
             for i in range(32):
                 image_line = testfile.readline()
                 for j in range(32):
-                    self.test_classes[idx][i][j] = image_line[j]
+                    self.testing_classes[idx][i][j] = image_line[j]
             image_line = testfile.readline()
             for ch in image_line:
-                if ch.isdigit() and int(ch) != label_list[idx]:
+                if ch.isdigit() and int(ch) != self.testing_labels[idx]:
                     print('TESTFILE ALIGN ERROR')
         testfile.close()
 
@@ -69,8 +69,8 @@ class Bayesian_Model:
                 denom = float('-inf')
             for i in range(32):
                 for j in range(32):
-                    for pixel in self.train_classes[num][i][j]:
-                        self.train_classes[num][i][j][pixel] = self.train_classes[num][i][j][pixel] + factor  - denom
+                    for pixel in self.training_classes[num][i][j]:
+                        self.training_classes[num][i][j][pixel] = self.training_classes[num][i][j][pixel] + factor  - denom
 
     def prior(self):
         total_count = sum(self.count)
@@ -87,7 +87,7 @@ class Bayesian_Model:
         largest_posterior = [[float('-inf'), " "] for i in range(10)]
         smallest_posterior = [[float('inf'), " "] for i in range(10)]
         line = 0
-        for label in label_list:
+        for label in self.testing_labels:
             maxi = float('-inf')
             mini = float('inf')
             predicted = 0
@@ -95,8 +95,8 @@ class Bayesian_Model:
                 possibility = self.priors[each_possibility]
                 for i in range(32):
                     for j in range(32):
-                        pixel = self.test_classes[each][i][j]
-                        possibility += self.train_classes[each_possibility][i][j][pixel]
+                        pixel = self.testing_classes[each][i][j]
+                        possibility += self.training_classes[each_possibility][i][j][pixel]
                 if possibility > maxi:
                     predicted = each_possibility
                     maxi = possibility
@@ -144,9 +144,9 @@ class Bayesian_Model:
         confusion_tuple.sort(key = lambda x: -x[1])
         
         for i in range(4):
-            feature1_pre = self.train_classes[confusion_tuple[i][0][0]]
+            feature1_pre = self.training_classes[confusion_tuple[i][0][0]]
             feature1 = [[chardict['1'] for chardict in row] for row in feature1_pre]
-            feature2_pre = self.train_classes[confusion_tuple[i][0][1]]
+            feature2_pre = self.training_classes[confusion_tuple[i][0][1]]
             feature2 = [[chardict['1'] for chardict in row] for row in feature2_pre]
 
             fig = [None for k in range(3)]
@@ -162,5 +162,4 @@ class Bayesian_Model:
                 plt.colorbar(heatmap[k])
                 # plt.show()
                 plt.savefig('src/binaryheatmap%.0f%d.png' % (i + 1, k + 1) )
-            
             
